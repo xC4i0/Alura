8d14c0f1f394e543847ae52276a18d8207b2e884
Caio_Carmo <128866704+xC4i0@users.noreply.github.com>
2024-05-10 17:32:29 -0300

    Desafios do modulo 3 resolvidos

--- logicaJsPrejeto/logica-js-projeto_inicial/app.js
+++ logicaJsPrejeto/logica-js-projeto_inicial/app.js
@@ -20,6 +20,57 @@ while (numberTyped != numberSecret) {
 }
 
 
+/*
+    DESAFIOS PROPOSTO
+
+1. Crie um contador que comece em 1 e vá até 10 usando um loop while. Mostre cada número.
+    let n1 = 1
+
+    while (n1 <= 10) {
+        console.log(n1)
+        n1++
+    }
+
+2. Crie um contador que começa em 10 e vá até 0 usando um loop while. Mostre cada número.
+    let n1 = 10
+
+    while (n1 >= 0) {
+        console.log(n1)
+        n1--
+    }
+
+3. Crie um programa de contagem regressiva. Peça um número e conte deste número até 0, usando um loop while no console do navegador.
+
+    let numberTyped = prompt('Digite um número qualquer')
+
+    while (numberTyped >= 0) {
+        console.log(numberTyped)
+        numberTyped--
+    }
+
+4. Crie um programa de contagem progressiva. Peça um número e conte de 0 até esse número, usando um loop while no console do navegador.
+
+    let numberTyped = prompt('Digite um número pra contagem: ')
+let firstNumber = 0
+
+while (firstNumber <= numberTyped) {
+    console.log(firstNumber)
+    firstNumber++
+}
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
 
 
 
