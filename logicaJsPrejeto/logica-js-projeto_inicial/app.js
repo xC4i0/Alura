29828e64164ccc521a3d4230e0a0880d1927a5be
Caio_Carmo <128866704+xC4i0@users.noreply.github.com>
2024-05-10 13:25:01 -0300

    Desafios do segundo modulo feitos

--- logicaJsPrejeto/logica-js-projeto_inicial/app.js
+++ logicaJsPrejeto/logica-js-projeto_inicial/app.js
@@ -40,6 +40,49 @@
 */
 
 
+/*
+
+1. Pergunte ao usuário qual é o dia da semana. Se a resposta for "Sábado" ou "Domingo", mostre "Bom fim de semana!". Caso contrário, mostre "Boa semana!".
+    const dayToday = prompt('Qual é o dia da semana?')
+
+    if (dayToday == 'Sabado' || dayToday == 'Domingo') {
+        alert('Bom dim fim de semana!')
+    } else {
+        alert('Boa semana!')
+    }
+
+2. Verifique se um número digitado pelo usuário é positivo ou negativo. Mostre um alerta informando.
+    const numberTyped = prompt('Digite um número negativo ou positivo')
+
+    if (numberTyped < 0) {
+        alert('Número negativo')
+    } else {
+        alert('Numero positvo')
+    }
+
+3. Crie um sistema de pontuação para um jogo. Se a pontuação for maior ou igual a 100, mostre "Parabéns, você venceu!". Caso contrário, mostre "Tente novamente para ganhar.".
+    const numberGame = prompt('Qual foi a sua pontuação?')
+
+    if (numberGame >= 100) {
+        alert('Parabéns, você venceu!')
+    } else {
+        alert('Tente novamente para ganhar.')
+    }
+
+4. Crie uma mensagem que informa o usuário sobre o saldo da conta, usando uma template string para incluir o valor do saldo.
+    const saldoConta = prompt('Digite o saldo da sua conta:')
+
+    alert(`O saldo da sua conta é de ${saldoConta}`)
+
+5. Peça ao usuário para inserir seu nome usando prompt. Em seguida, mostre um alerta de boas-vindas usando esse nome.
+    const myName = prompt('Digite seu nome:')
+
+    alert(`Bem vindo ${myName}`)
+
+*/
+
+
+
 
 
 
