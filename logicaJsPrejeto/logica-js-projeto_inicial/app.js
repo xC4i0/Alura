0c54226cee8f1086eebe54877cba5d6f6a441017
Caio_Carmo <128866704+xC4i0@users.noreply.github.com>
2024-05-10 00:13:37 -0300

    Alguns desafios feitos durante a aula

---
+++ logicaJsPrejeto/logica-js-projeto_inicial/app.js
@@ -0,0 +1,45 @@
+/*
+    DESAFIOS PROPOSTO
+
+1. Mostre um alerta com a mensagem "Boas vindas ao nosso site!".
+    alert('"Boas vindas ao nosso site!"')
+
+2. Declare uma variável chamada nome e atribua a ela o valor "Lua".
+    const name = 'lua'
+
+3. Crie uma variável chamada idade e atribua a ela o valor 25.
+    const idade = 25
+
+4. Defina uma variável numeroDeVendas e atribua a ela o valor 50.
+    const numeroDeVendas = 50
+
+5. Defina uma variável saldoDisponivel e atribua a ela o valor 1000.
+    const saldoDisponivel = 1000
+
+6. Exiba um alerta com o texto "Erro! Preencha todos os campos"  
+    alert("Erro! Preencha todos os campos")
+
+7. Declare uma variável chamada mensagemDeErro e atribua a ela o valor "Erro! Preencha todos os campos" Agora exiba um alerta com o valor da variável mensagemDeErro.
+    const mensagemDeErro = "Erro! Preencha todos os campos"
+    alert(mensagemDeErro)
+
+8. Use um prompt para perguntar o nome do usuário e armazená-lo na variável nome.
+    const yourName = prompt('Qual seu nome?')
+
+9. Peça ao usuário para digitar sua idade usando um prompt e armazene-a na variável idade.
+    const yourAge = prompt('Qual é sua idade?')
+
+10. Agora, caso a idade seja maior ou igual que 18, exiba um alerta com a mensagem "Pode tirar a habilitação!".
+        const yourAge = prompt('Qual é sua idade?')
+    if (yourAge >= 18) {
+        alert('Você ja pode ter sua HABILITAÇÃO')
+    } else {
+        alert('Você ainda não pode tirar a HABILITAÇÃO')
+    }
+
+*/
+
+
+
+
+
